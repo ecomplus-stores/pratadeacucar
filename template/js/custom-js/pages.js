5b87e71f81318786a43794c31709d62b74968084
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-09-12 17:49:01 -0300

    chore: avoid display none

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,19 +1,4 @@
 // Add your custom JavaScript for storefront pages here.
-const readItems = setInterval(() => {
-    let items = $('.product-card--inactive')
-    let count = 0
-    let max = items.length
-    if (items.length) {
-        for (let item in items) {
-            count++
-            if (max === count) {
-                clearInterval(readItems)
-                break
-            }
-            let $item = items[item]
-            $item.parentNode.remove()
-        }
-    }
-}, 500)
+
 
 
