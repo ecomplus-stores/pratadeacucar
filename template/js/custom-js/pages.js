be40271fec9acf107ff78d1bca45119370085e9a
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-09-13 21:30:18 -0300

    Update pages.js

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,4 +1,17 @@
 // Add your custom JavaScript for storefront pages here.
 
-
+const readItems = setInterval(() => {
+    let items = $('.product-card--inactive')
+    let count = 0
+    let max = items.length
+    if (items.length) {
+        for (let item in items) {
+            count++
+            let $item = items[item]
+	    if ($item.parentNode) {
+		$item.parentNode.remove()
+            }
+        }
+    }
+}, 500)
 
